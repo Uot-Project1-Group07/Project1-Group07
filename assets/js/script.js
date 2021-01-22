4fa1c5706677362f947e5be1a5a7705da71534a6
Esteban Romero <esroleo@gmail.com>
2021-01-21 22:36:57 -0500

    drop down buttons added

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+// var instance = M.FormSelect.getInstance(elem);
+
+// instance.getSelectedValues();
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+// var elems = document.querySelectorAll('select');
+// var instances = M.FormSelect.init(elems, options);
+// });
+
+$('.dropdown-trigger').dropdown();
