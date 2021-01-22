d1d5dd7f73442df8ec4ce336fd5c370f16ffbc40
anagha778 <anagha778@gmail.com>
2021-01-22 16:24:04 -0500

    js file for joke section added

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+//define global variables
+var buttonEl = document.querySelector("#clickme");
+var jokeDivEl = document.querySelector(".joke");
+//load on pageload
+document.addEventListener("DOMContentLoaded", displayJock);
+
+//function to load a random joke data
+async function displayJock() {
+  var jokeUrl = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json"
+    }
+  });
+  //wait till response is ok
+  var jokeObj = await jokeUrl.json();
+  jokeDivEl.innerHTML = jokeObj.joke;
+};
+
+//load more jokes on click of click me button
+buttonEl.addEventListener("click", displayJock);
