d6e29a6074aedcdc53cf062c5fe8813b8337ffd0
Esteban Romero <esroleo@gmail.com>
2021-01-22 13:40:34 -0500

    button dropdown works using semantic drop down

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,46 @@
+$(document).ready(function() { // Wait for the DOM to be ready for window., javascript/jquery and others
 
+
+        alert(
+        $('#select')
+        .dropdown('get value')
+        );
+
+let citiesContainerEl = document.querySelector("#test123")
+
+console.log("container EL Found " + citiesContainerEl)
+  
+
+// Funciton to handle the event from the list of cities when clicked.
+var cityClicked = function (event) {
+
+    let citySelectredDropDownEl = 
+    $('#select')
+    .dropdown('get value')
+
+    alert(citySelectredDropDownEl);
+
+    //let cityClicked = event.target.getAttribute("data-city")
+    // if (cityClicked){
+    //     //getWeatherData(event, cityClicked);
+    //     alert(cityClicked)
+    // } else { // If the value is empty, it should not happen but it is a failsafe.
+    //     alert(cityClicked)
+    //     alert("Internal erro found, please email esroleo@gmail.com.\nPlease provide story of issue in order for it to be fixed");
+    // };
+};
+
+
+
+// Event listener for the cities incase they are clicked.
+//citiesListContainerEl.addEventListener("click",  cityClicked);
+citiesContainerEl.addEventListener("click", cityClicked)
+
+
+        
+
+
+});
 // var instance = M.FormSelect.getInstance(elem);
 
 // instance.getSelectedValues();
@@ -8,5 +50,3 @@
 // var elems = document.querySelectorAll('select');
 // var instances = M.FormSelect.init(elems, options);
 // });
-
-$('.dropdown-trigger').dropdown();
