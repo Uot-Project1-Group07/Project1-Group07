648e20525fbe801004b8212ce2adb4a2735b2465
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-01-23 16:32:10 -0500

    datepicker disappears after selecting and dates are generated

--- script.js
+++ script.js
@@ -2,12 +2,10 @@
 //The area where the data is formed
 var dateArea = document.querySelector("#dates_info");
 
-const Calander = document.querySelector('.datepicker');
-M.datePicker.init(Calander,{
-    format:'dd-mm-yy'
-});
+const input = document.querySelector('input');
 
-var dates = function (e){
+
+var dates = function (){
     var day1 = document.createElement("li");
     day1.innerHTML = "<h5>"+ "Day 1"+ "</h5>" + 
     "The first symptoms begin from two to 14 days after you have been exposed to the virus. A new study from Johns Hopkins Bloomberg School of Public Health suggests a median time of about five days.";
@@ -18,8 +16,12 @@ var dates = function (e){
     "The average duration of fever was 12 days. Ninety-nine percent of the patients studied had a fever. About 50% felt fatigued and had a dry cough, with 33% having difficulty breathing and complaining of muscle pain.";
     dateArea.appendChild(day2);   
 };
+
 var datePicker = function(e){
-    alert("I am here");
+    var log = e.target.value;
+    
+    dates();
+    input.hidden = true;
 }
 
 input.addEventListener('input', datePicker);
