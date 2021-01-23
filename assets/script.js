0851295851f9525fb6051543a7c6151accc92a77
Kris Pennimpede <kpennimpede@hotmail.com>
2021-01-23 14:15:11 -0500

    can select and display stats, updates on selection change

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,33 @@
-var citySelectEl = document.querySelector("#city-select");
+var areaSelectEl = document.querySelector("#area-select");
 var canadaRecoveredEl = document.querySelector("#CArecovered");
 var canadaVaccinatedEl = document.querySelector("#CAvaccinated");
 var canadaRecoveredNewEl = document.querySelector("#CArecoverednew");
 var canadaVaccineNewEl = document.querySelector("#CAvaccinatednew");
+var areaStatsEl = document.querySelector("#area-stats");
 
-var getCityData = function(city) {
-  var citySearchUrl = "https://api.opencovid.ca/summary?loc=" + city;
+
+//get data for slected area
+var getAreaData = function(area) {
+  var areaSearchUrl = "https://api.opencovid.ca/summary?loc=" + area;
   
-  fetch(citySearchUrl).then(function(response) {
+  fetch(areaSearchUrl).then(function(response) {
     response.json().then(function(data) {
-      console.log(data);
+      //console.log(data);
+      displayAreaData(data);
     })
   })
 }
 
-var regionSelectHandler  = function(event) {
+
+//update stats when selection changes
+var areaSelectHandler  = function(event) {
   event.preventDefault();
-  var cityCode = citySelectEl.value;
-  console.log(cityCode);
-  getCityData(cityCode);
+  var areaCode = areaSelectEl.value;
+  //console.log(areaCode);
+  getAreaData(areaCode);
 }
 
+//get Canada wide stats and display them
 var getCanadaData = function() {
   fetch("https://api.opencovid.ca").then(function(response) {
     response.json().then(function(data) {
@@ -37,13 +44,50 @@ var getCanadaData = function() {
   })
 }
 
+var displayAreaData = function(data) {
+  // create data variables
+  var areaName = data.summary[0].province;
+  var areaRecoveredNew = data.summary[0].recovered;
+  var areaRecovered = data.summary[0].cumulative_recovered;
+  var areaVaccineNew = data.summary[0].avaccine;
+  var areaVaccinated = data.summary[0].cumulative_avaccine;
+  
+  //clear old data
+  areaStatsEl.textContent = ""
+  
+  //create stat title element
+  var areaTitleEl = document.createElement("h6");
+  areaTitleEl.textContent = areaName + " Stats";
+  areaStatsEl.appendChild(areaTitleEl);
+
+  //create new recoveries element
+  var newRecoveriesEl = document.createElement("p");
+  newRecoveriesEl.textContent = "New Recoveries: " + areaRecoveredNew;
+  areaStatsEl.appendChild(newRecoveriesEl);
+
+  //create total recoveries element
+  var totalRecoveriesEl = document.createElement("p");
+  totalRecoveriesEl.textContent = "Total Recovered: " + areaRecovered;
+  areaStatsEl.appendChild(totalRecoveriesEl);
+
+  //create new vaccinations element
+  var newVaccinationsEl = document.createElement("p");
+  newVaccinationsEl.textContent = "New Vaccinations: " + areaVaccineNew;
+  areaStatsEl.appendChild(newVaccinationsEl);
+
+  //create total vaccinations element
+  var totalVaccinationsEl = document.createElement("p");
+  totalVaccinationsEl.textContent = "Total Vaccinations: " + areaVaccinated;
+  areaStatsEl.appendChild(totalVaccinationsEl); 
+}
+
 
 //document.addEventListener('DOMContentLoaded', function() {
 //  var elems = document.querySelectorAll('select');
 //  var instances = M.FormSelect.init(elems, options);
 //});
 
-citySelectEl.addEventListener("change", regionSelectHandler);
+areaSelectEl.addEventListener("change", areaSelectHandler);
 
-getCityData(3595);
+
 getCanadaData();
