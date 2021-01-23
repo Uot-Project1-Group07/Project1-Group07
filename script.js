e0c2d5da1d05afac21d536d943954f69118eba23
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-01-23 15:54:49 -0500

    initial comments area set up

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 
-var buttonClick = document.querySelector("#clicked");
-
 //The area where the data is formed
 var dateArea = document.querySelector("#dates_info");
 
-var dates = function (){
+const Calander = document.querySelector('.datepicker');
+M.datePicker.init(Calander,{
+    format:'dd-mm-yy'
+});
+
+var dates = function (e){
     var day1 = document.createElement("li");
-    day1.innerHTML = "1" + 
+    day1.innerHTML = "<h5>"+ "Day 1"+ "</h5>" + 
     "The first symptoms begin from two to 14 days after you have been exposed to the virus. A new study from Johns Hopkins Bloomberg School of Public Health suggests a median time of about five days.";
     dateArea.appendChild(day1);
 
     var day2 = document.createElement("li");
-    day2.innerHTML = "2" + 
+    day2.innerHTML = "<h5>"+ "Day 2"+ "</h5>" +   
     "The average duration of fever was 12 days. Ninety-nine percent of the patients studied had a fever. About 50% felt fatigued and had a dry cough, with 33% having difficulty breathing and complaining of muscle pain.";
     dateArea.appendChild(day2);   
 };
+var datePicker = function(e){
+    alert("I am here");
+}
 
-
-buttonClick.addEventListener("click",dates);
-//disable the clicked button after once clicked
-buttonClick.disabled = true;
+input.addEventListener('input', datePicker);
