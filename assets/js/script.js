cc8a5f64a72e577ebc66e3a0be59519e1e5bc100
anagha778 <anagha778@gmail.com>
2021-01-23 01:11:57 -0500

    added error handling code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,20 @@ document.addEventListener("DOMContentLoaded", displayJock);
 
 //function to load a random joke data
 async function displayJock() {
+try
+  {
   var jokeUrl = await fetch("https://icanhazdadjoke.com/", {
     headers: {
-      Accept: "application/json"
-    }
-  });
-  //wait till response is ok
-  var jokeObj = await jokeUrl.json();
-  jokeDivEl.innerHTML = jokeObj.joke;
+      Accept: "application/json"}
+    })
+    //wait till response is ok
+    var jokeObj = await jokeUrl.json();
+    jokeDivEl.innerHTML = jokeObj.joke;
+  }
+  catch(err)
+  {
+    console.log(err);
+  }
 };
 
 //load more jokes on click of click me button
