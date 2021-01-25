49e6d8439a634dce20f7fa857d366217208566c8
Esteban Romero <esroleo@gmail.com>
2021-01-24 19:58:38 -0500

    Kris fixes integrated into the city area

--- assets/js/script.js
+++ assets/js/script.js
@@ -187,41 +187,49 @@ var getCanadaData = function() {
 }
 
 var displayAreaData = function(data) {
-  // create data variables
-  var areaName = data.summary[0].province;
-  var areaRecoveredNew = data.summary[0].recovered;
-  var areaRecovered = data.summary[0].cumulative_recovered;
-  var areaVaccineNew = data.summary[0].avaccine;
-  var areaVaccinated = data.summary[0].cumulative_avaccine;
+    // create data variables
+    var areaName = data.summary[0].province;
+    var areaRecoveredNew = data.summary[0].recovered;
+    var areaRecovered = data.summary[0].cumulative_recovered;
+    var areaVaccineNew = data.summary[0].avaccine;
+    var areaVaccinated = data.summary[0].cumulative_avaccine;
+    
+    //clear old data
+    areaStatsEl.textContent = ""
+    
+    //create stat title element
+    var areaTitleEl = document.createElement("p");
+    areaTitleEl.classList.add("btn", "btn-city", "green", "accent-4");
+    areaTitleEl.textContent = areaName + " Stats";
+    areaStatsEl.appendChild(areaTitleEl);
   
-  //clear old data
-  areaStatsEl.textContent = ""
+    //create new recoveries element
+    var newRecoveriesEl = document.createElement("p");
+    newRecoveriesEl.classList = ("btn btn-city waves-effect waves-light") ;
+    newRecoveriesEl.textContent = "New Recoveries: " + areaRecoveredNew;
+    areaStatsEl.appendChild(newRecoveriesEl);
   
-  //create stat title element
-  var areaTitleEl = document.createElement("h6");
-  areaTitleEl.textContent = areaName + " Stats";
-  areaStatsEl.appendChild(areaTitleEl);
-
-  //create new recoveries element
-  var newRecoveriesEl = document.createElement("p");
-  newRecoveriesEl.textContent = "New Recoveries: " + areaRecoveredNew;
-  areaStatsEl.appendChild(newRecoveriesEl);
-
-  //create total recoveries element
-  var totalRecoveriesEl = document.createElement("p");
-  totalRecoveriesEl.textContent = "Total Recovered: " + areaRecovered;
-  areaStatsEl.appendChild(totalRecoveriesEl);
-
-  //create new vaccinations element
-  var newVaccinationsEl = document.createElement("p");
-  newVaccinationsEl.textContent = "New Vaccinations: " + areaVaccineNew;
-  areaStatsEl.appendChild(newVaccinationsEl);
-
-  //create total vaccinations element
-  var totalVaccinationsEl = document.createElement("p");
-  totalVaccinationsEl.textContent = "Total Vaccinations: " + areaVaccinated;
-  areaStatsEl.appendChild(totalVaccinationsEl); 
-}
+    //create total recoveries element
+    var totalRecoveriesEl = document.createElement("p");
+    //totalRecoveriesEl.classList.add("btn", "btn-city");
+    totalRecoveriesEl.classList = ("btn btn-city waves-effect waves-light") ;
+    totalRecoveriesEl.textContent = "Total Recovered: " + areaRecovered;
+    areaStatsEl.appendChild(totalRecoveriesEl);
+  
+    //create new vaccinations element
+    var newVaccinationsEl = document.createElement("p");
+    //newVaccinationsEl.classList.add("btn", "btn-city");
+    newVaccinationsEl.classList = ("btn btn-city waves-effect waves-light") ;
+    newVaccinationsEl.textContent = "New Vaccinations: " + areaVaccineNew;
+    areaStatsEl.appendChild(newVaccinationsEl);
+  
+    //create total vaccinations element
+    var totalVaccinationsEl = document.createElement("p");
+    //totalVaccinationsEl.classList.add("btn", "btn-city");
+    totalVaccinationsEl.classList = ("btn btn-city waves-effect waves-light") ;
+    totalVaccinationsEl.textContent = "Total Vaccinations: " + areaVaccinated;
+    areaStatsEl.appendChild(totalVaccinationsEl); 
+  }
 
 
 //document.addEventListener('DOMContentLoaded', function() {
