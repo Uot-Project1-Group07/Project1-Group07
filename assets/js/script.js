1438585468995b29a79929c8a27d3d091a2fc135
Esteban Romero <esroleo@gmail.com>
2021-01-24 19:27:35 -0500

    Raj's inspirational quotes added

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,6 +105,38 @@ buttonEl.addEventListener("click", displayJock);
 // *** End of Anagha's Section for Jokes API *** //
 
 
+// *** Start of Raj's Inspirational Quotes API *** //
+
+var quoteApiUrl = "https://type.fit/api/quotes";
+var inspoDivEl = document.querySelector("#inspo-refresh");
+
+var getQuotes = function () {
+  fetch(quoteApiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        var randIndex = Math.floor(Math.random() * 100);
+        var quote = data[randIndex].text;
+        var authorName = data[randIndex].author;
+        // Place text onto HTML page
+        document.getElementById("quote-display").textContent =
+          "'" + quote + "'";
+        document.getElementById("author-display").textContent =
+          "- " + authorName;
+      });
+    }
+  });
+};
+
+//run function on load to prepopulate quote
+getQuotes();
+//event listener to load more quotes
+inspoDivEl.addEventListener("click", getQuotes);
+
+// *** End of Raj's Inspirational Quotes API *** //
+
+
+
+
 
 // *** Kris Section for Cities API *** //
 
