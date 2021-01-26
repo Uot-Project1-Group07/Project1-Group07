b7425cf982ca0efa17c075d771d545e204b72468
Rajendra Dhanraj <rajiedhan@Rajendras-MacBook-Pro.local>
2021-01-25 19:38:17 -0500

    local storage

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,7 +12,21 @@ var formLoginHandler = function (event) {
   var userPassword = userPasswordEl.value.trim();
   //split password to indentify age group // Password format ("password-age")
   var userAge = userPassword.split("-")[1];
+  //put user info into object and store to local
+  var userInfoObj = { iD: username, age: userAge };
+  userInfo.push(userInfoObj);
+  window.localStorage.setItem("user-info", JSON.stringify(userInfo));
 
+  //check for age and display message indicating page user will be sent to.
+  if (userAge <= 12) {
+    alert('You will be directed to the "Kids" page!');
+  } else if (userAge >= 13 && userAge <= 19) {
+    alert('You will be directed to the "Teenagers" page!');
+  } else if (userAge >= 20 && userAge <= 64) {
+    alert('You will be directed to the "Adults" page!');
+  } else {
+    alert('You will be directed to the "Seniors" page!');
+  }
   // optional hide form after login
   loginForm.style.display = "none";
   //redirect to main page *****Conf path correct******
